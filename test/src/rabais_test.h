128919ba21af369e8c3f10de3b86ab12dcb949b8
Thomas Charbonneau <tomtom.tc@hotmail.ca>
2019-03-29 19:52:12 -0400

    Clean up crew

--- test/src/rabais_test.h
+++ test/src/rabais_test.h
@@ -8,20 +8,13 @@ class GetRabaisTest : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(GetRabaisTest);
     CPPUNIT_TEST(test_dumb);
-    //CPPUNIT_TEST(test_code_client_plus_petit);
     CPPUNIT_TEST(test_code_client_plus_grand);
     CPPUNIT_TEST(test_code_postal_G0X);
     CPPUNIT_TEST(test_code_postal_H4L);
     CPPUNIT_TEST(test_code_postal_H2X);
-    //CPPUNIT_TEST(test_code_postal_invalide);
     CPPUNIT_TEST(test_age_client);
     CPPUNIT_TEST(test_date_adhesion);
     CPPUNIT_TEST(test_montant_facture);
-
-
-    /*
-    CPPUNIT_TEST(test_code_client_plus_grand);
-    */
     CPPUNIT_TEST_SUITE_END();
     
 private:
@@ -33,11 +26,9 @@ public:
     void tearDown();
     
     // Fonctions de tests
-    //void test_code_postal_invalide();
     void test_code_postal_G0X();
     void test_code_postal_H4L();
     void test_code_postal_H2X();
-    //void test_code_client_plus_petit();
     void test_code_client_plus_grand();
     void test_code_client_egal();
     void test_age_client();
